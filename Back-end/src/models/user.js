919bb15fd879d14eb20fc11dc943afefeb36ac30
shamirchulliyan <103748768+shamirchulliyan@users.noreply.github.com>
2022-12-18 19:40:59 +0530

    change in model and router

--- Back-end/src/models/user.js
+++ Back-end/src/models/user.js
@@ -71,5 +71,14 @@ const userSchema = new mongoose.Schema({
     timestamps : true
 });
 
+//Generate auth token
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString()}, process.env.JWT_SECRET);
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
+ };
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
